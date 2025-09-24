8cb75861b3f1d2683bae89c002a5c0db46bb2eff
Milad <milad.rahmat@gmail.com>
2025-09-25 01:09:16 +0300

    Completed up to exercise 2.20

--- part2/countriesdata/src/components/OneCountry.jsx
+++ part2/countriesdata/src/components/OneCountry.jsx
@@ -1,5 +1,19 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const OneCountry = ({ country }) => {
+	const api_key = import.meta.env.VITE_SOME_KEY
+	const [weather, setWeather] = useState(null)
+	
+	useEffect(() => {
+		axios
+		.get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&units=metric&appid=${api_key}`)
+		.then(response => {
+			setWeather(response.data)
+		})
+	}, [])
+
+
   return (
     <div>
       <div>
@@ -22,6 +36,16 @@ const OneCountry = ({ country }) => {
       <div>
         <img src={country.flags.png} alt={`${country.name.common} flag`} />
       </div>
+		<h2>Weather in {country.capital}</h2>
+		{weather ? (
+			<div>
+				<p>Temperature {weather.main.temp} Celsius</p>
+				<img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`} />
+				<p>Wind {weather.wind.speed} m/s</p>
+			</div>
+		) : (
+			<p>Loading weather...</p>
+		)}
     </div>
   )
 }
